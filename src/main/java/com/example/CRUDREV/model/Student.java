a397a673cf55fb5c19872ba2817e6858cdbcc319
Debadyuti <debadyuti.karmakar@gmail.com>
2023-05-21 15:54:02 +0530

    Student entity

--- src/main/java/com/example/CRUDREV/model/Student.java
+++ src/main/java/com/example/CRUDREV/model/Student.java
@@ -1,11 +1,30 @@
 package com.example.CRUDREV.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name="studentdb")
 public class Student {
     private String studentId;
     private String studentName;
     private String emailId;
     private String department;
 
+
+    public Student() {
+
+    }
+
+    public Student(String studentId, String studentName, String emailId, String department) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.emailId = emailId;
+        this.department = department;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public String getStudentId() {
         return studentId;
     }
@@ -14,6 +33,7 @@ public class Student {
         this.studentId = studentId;
     }
 
+    @Column(name = "name", nullable = false)
     public String getStudentName() {
         return studentName;
     }
@@ -21,7 +41,7 @@ public class Student {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
-
+    @Column(name = "emailid", nullable = false)
     public String getEmailId() {
         return emailId;
     }
@@ -30,6 +50,7 @@ public class Student {
         this.emailId = emailId;
     }
 
+    @Column(name = "department", nullable = false)
     public String getDepartment() {
         return department;
     }
