1d733b1ec22c5f72469f10619c558d7c4648c6f4
Debadyuti <debadyuti.karmakar@gmail.com>
2023-06-15 18:43:42 +0530

    Update student

--- src/main/java/com/example/CRUDREV/controller/StudentController.java
+++ src/main/java/com/example/CRUDREV/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.example.CRUDREV.model.Student;
 import com.example.CRUDREV.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,6 +41,21 @@ public class StudentController {
                 .orElseThrow(() -> new ResourceNotFound(("Student not found for this id :"+id)));
         return ResponseEntity.ok().body(student);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable(value = "id") String studentId, @RequestBody Student studentDetails) throws ResourceNotFound {
+        Student student = studentRepo.findById(String.valueOf(studentId)).orElseThrow(() -> new ResourceNotFound("Student not found"));
+
+        student.setStudentName(studentDetails.getStudentName());
+        student.setDepartment(studentDetails.getDepartment());
+        student.setEmailId(studentDetails.getEmailId());
+
+        final Student updateStudent = studentRepo.save(student);
+        return ResponseEntity.ok(updateStudent);
+
+
+    }
+
 
 
 
